69905da1143edade639c49c5c5e13c66a9f2fe07
mohd soaib <soaibpayam@gmail.com>
2025-08-20 16:49:19 +0530

    Updated project with latest changes

--- backend/server.js
+++ backend/server.js
@@ -205,7 +205,7 @@ const path = require("path");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
